9fba0c1470f41aaa603eb529ca324b2724536086
Ramesh <rameshraja1011@gmail.com>
2026-03-05 19:31:17 +0530

    full project

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,7 +6,6 @@ import Services from "./components/Services";
 import ProductDetails from "./components/ProductDetails";
 import Testimonialpage from "./pages/Testimonialpage";
 import Contact from "./pages/Contactpage";
-import ProductsPage from "./pages/ProductsPage";
 import Aboutus from "./pages/Aboutus";
 import PreBooking from "./components/PreBooking";
 import AdminDashboard from "./components/AdminDashboard";
@@ -22,7 +21,6 @@ function App() {
         <Route path="/product/:id" element={<ProductDetails />} />
         <Route path="/testimonial" element={<Testimonialpage />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/products" element={<ProductsPage />} />
         <Route path="/aboutus" element={<Aboutus />} />
        <Route path="/prebooking/:id" element={<PreBooking />} />
 
