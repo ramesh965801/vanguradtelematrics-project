6af5b2bf6dc6c0f21833891907c6c7310012d511
Ramesh Biswal <143710115+ramesh965801@users.noreply.github.com>
2026-03-10 22:46:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+app.use("/uploads", express.static("uploads"));
 
 const adminRoutes = require("./routes/admin");
 const prebookingRoutes = require("./routes/prebooking");
