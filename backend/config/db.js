7d19e980efe908fe1dccfb082767d2501d349ba2
Ramesh Biswal <143710115+ramesh965801@users.noreply.github.com>
2026-03-10 16:06:42 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,13 @@ const mysql = require("mysql2");
 
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: process.env.DB_PORT || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: { rejectUnauthorized: false },
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 db.connect((err) => {
