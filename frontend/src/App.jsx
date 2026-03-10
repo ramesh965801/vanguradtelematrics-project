8d44b6ea81b30859e3921d4c63b4de3be73ce46e
Ramesh Biswal <143710115+ramesh965801@users.noreply.github.com>
2026-03-10 23:15:51 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Services from "./components/Services";
