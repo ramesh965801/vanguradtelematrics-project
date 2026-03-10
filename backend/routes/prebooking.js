2a06e3e99f9c617dce82e69d1c2cd3066ce6b89a
Ramesh Biswal <143710115+ramesh965801@users.noreply.github.com>
2026-03-10 22:16:32 +0530

    Update prebooking.js

--- backend/routes/prebooking.js
+++ backend/routes/prebooking.js
@@ -2,37 +2,34 @@ const express = require("express");
 const router = express.Router();
 const db = require("../config/db");
 
-// POST PreBooking
 router.post("/", (req, res) => {
 
   const { product_id, name, email, phone, quantity, address } = req.body;
 
-  if (!product_id || !name || !email || !phone || !quantity || !address) {
-    return res.status(400).json({ success: false, message: "All fields required" });
-  }
-
   const sql = `
     INSERT INTO prebookings
-    (product_id, name, email, phone, quantity, address)
-    VALUES (?, ?, ?, ?, ?, ?)
+    (product_id,name,email,phone,quantity,address)
+    VALUES (?,?,?,?,?,?)
   `;
 
   db.query(
     sql,
     [product_id, name, email, phone, quantity, address],
     (err, result) => {
+
       if (err) {
-        console.error("DB Error:", err);
-        return res.status(500).json({ success: false, message: "Database error" });
+        console.error(err);
+        return res.status(500).json({ success: false });
       }
 
       res.json({
         success: true,
-        bookingId: result.insertId,
-        message: "Prebooking saved successfully"
+        bookingId: result.insertId
       });
+
     }
   );
+
 });
 
 module.exports = router;
