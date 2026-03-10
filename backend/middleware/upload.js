1a6fcc416b09d6219c52a78c7ff11a2008175add
Ramesh Biswal <143710115+ramesh965801@users.noreply.github.com>
2026-03-10 15:01:19 +0530

    Update upload.js

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,13 +1,13 @@
-// backend/middleware/upload.js
 const multer = require("multer");
 const path = require("path");
 
 const storage = multer.diskStorage({
-  destination: "./uploads",
+  destination: "./uploads", // ensure this folder exists on Render
   filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
+    cb(null, Date.now() + path.extname(file.originalname)); // unique filenames
   },
 });
 
 const upload = multer({ storage });
+
 module.exports = upload;
