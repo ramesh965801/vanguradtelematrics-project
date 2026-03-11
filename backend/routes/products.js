1321f07dd9a81845ae7d4b5bc4d259491d8e8ccd
Ramesh Biswal <143710115+ramesh965801@users.noreply.github.com>
2026-03-11 16:44:37 +0530

    Update products.js

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -2,22 +2,33 @@ const express = require("express");
 const router = express.Router();
 const db = require("../config/db");
 
-// Get all products
+// GET ALL PRODUCTS
 router.get("/products", (req, res) => {
+
   const sql = "SELECT * FROM products ORDER BY id DESC";
 
   db.query(sql, (err, result) => {
-    if (err) return res.status(500).json({ error: "Database error" });
 
-    const products = result.map((product) => ({
-      ...product,
-      image_url: product.image
-        ? `${process.env.BASE_URL}/uploads/${product.image}`
-        : null
-    }));
+    if (err) {
+      console.log(err);
+      return res.status(500).json({ error: "Database error" });
+    }
+
+    const products = result.map((product) => {
+
+      return {
+        ...product,
+        image_url: product.image
+          ? `${process.env.BASE_URL}/uploads/${product.image}`
+          : null
+      };
+
+    });
 
     res.json(products);
+
   });
+
 });
 
 module.exports = router;
