0d87a792b47e81c91801b2074170796a6181e0fa
Ramesh Biswal <143710115+ramesh965801@users.noreply.github.com>
2026-03-11 20:51:27 +0530

    Update admin.js

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -1,1 +1,15 @@
-const express = require("express"); const router = express.Router(); const upload = require("../middleware/upload"); const adminController = require("../controllers/adminController"); router.post("/add-product", upload.single("image"), adminController.addProduct); router.get("/products", adminController.getProducts); router.delete("/delete-product/:id", adminController.deleteProduct); router.get("/prebookings", adminController.getPrebookings); module.exports = router;
+const express = require("express");
+const router = express.Router();
+
+const upload = require("../middleware/upload");
+const adminController = require("../controllers/adminController");
+
+router.post("/add-product", upload.single("image"), adminController.addProduct);
+
+router.get("/products", adminController.getProducts);
+
+router.delete("/delete-product/:id", adminController.deleteProduct);
+
+router.get("/prebookings", adminController.getPrebookings);
+
+module.exports = router;
