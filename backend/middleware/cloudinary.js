d9075007b6917cb248cbe6431ccc55e76353c618
Ramesh Biswal <143710115+ramesh965801@users.noreply.github.com>
2026-03-11 17:34:24 +0530

    Create cloudinary.js

---
+++ backend/middleware/cloudinary.js
@@ -0,0 +1,19 @@
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const multer = require("multer");
+
+cloudinary.config({
+  cloud_name: "YOUR_CLOUD_NAME",
+  api_key: "YOUR_API_KEY",
+  api_secret: "YOUR_API_SECRET",
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary,
+  params: {
+    folder: "vanguard-products",
+    allowed_formats: ["jpg", "png", "jpeg"],
+  },
+});
+
+module.exports = multer({ storage });
