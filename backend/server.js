25b740202c25e429dfa604d9830fa58c5fc49e43
Ramesh Biswal <143710115+ramesh965801@users.noreply.github.com>
2026-03-11 16:45:08 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -8,23 +8,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Serve static uploads
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-// Routes
 const adminRoutes = require("./routes/admin");
 const prebookingRoutes = require("./routes/prebooking");
 const productRoutes = require("./routes/products");
 
+app.use("/api", productRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/prebooking", prebookingRoutes);
-app.use("/api", productRoutes);
 
 app.get("/", (req, res) => {
   res.send("Backend Running");
 });
 
 const PORT = process.env.PORT || 8080;
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
