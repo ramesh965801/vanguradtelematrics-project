89099a40362533d76a813b3a1c8ce683da36a940
Ramesh Biswal <143710115+ramesh965801@users.noreply.github.com>
2026-03-11 16:43:12 +0530

    Update upload.js

--- backend/middleware/upload.js
+++ backend/middleware/upload.js
@@ -1,21 +1,16 @@
 const multer = require("multer");
 const path = require("path");
-const fs = require("fs");
-
-const uploadDir = path.join(__dirname, "../uploads");
-
-// Ensure uploads folder exists
-if (!fs.existsSync(uploadDir)) {
-  fs.mkdirSync(uploadDir);
-}
 
 const storage = multer.diskStorage({
+
   destination: (req, file, cb) => {
-    cb(null, uploadDir);
+    cb(null, path.join(__dirname, "../uploads"));
   },
+
   filename: (req, file, cb) => {
     cb(null, Date.now() + path.extname(file.originalname));
   }
+
 });
 
 module.exports = multer({ storage });
