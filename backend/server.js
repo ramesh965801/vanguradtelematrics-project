3ae898f1c41274c18612aa150610aaf6bf0675fa
Ramesh Biswal <143710115+ramesh965801@users.noreply.github.com>
2026-03-13 10:00:49 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -1,29 +1,46 @@
 require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
 
 const app = express();
 
+//razorpay integrated
+
+const paymentRoutes = require("./routes/paymentRoutes");
+
+
+// Middleware
 app.use(cors());
 app.use(express.json());
 
+app.use("/api/payment", paymentRoutes);
+
+// Static folder for product images
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
-const adminRoutes = require("./routes/admin");
+// Import Routes
 const prebookingRoutes = require("./routes/prebooking");
 const productRoutes = require("./routes/products");
+const adminRoutes = require("./routes/admin");
+const bookingRoutes = require("./routes/bookingRoutes");
 
-app.use("/api", productRoutes);
-app.use("/api/admin", adminRoutes);
+app.use("/api", bookingRoutes);
+
+// Use Routes
 app.use("/api/prebooking", prebookingRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/admin", adminRoutes);
 
+// Test Route
 app.get("/", (req, res) => {
-  res.send("Backend Running");
+  res.send("🚀 Backend Server Running");
 });
 
-const PORT = process.env.PORT || 8080;
+// Start Server
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`🚀 Server running on port ${PORT}`);
 });
