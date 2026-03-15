3f6b3a61e6e49b316ee0c48df6b85947cd3334ce
Ramesh Biswal <143710115+ramesh965801@users.noreply.github.com>
2026-03-15 15:24:27 +0530

    Update products.js

--- frontend/src/data/products.js
+++ frontend/src/data/products.js
@@ -4,7 +4,7 @@ const products = [
   {
     id: 1,
     title: "Vehicle Accident Tracker",
-    price: 4999,
+    price: 5,
     description:
       "Vehicle accident trackers in India utilize the Integrated Road Accident Database (iRAD) system.",
     image: product1
